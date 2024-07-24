9acb5b74345ffd8081af99877e86600c69c1929e
origin <mh.fouladi@hotmail.com>
2024-07-24 15:32:36 -0400

    Splash screen added

---
+++ MohammadAliModule10/src/main/java/mohammadali/fouladi/n01547173/mf/MoeFouSplash.java
@@ -0,0 +1,27 @@
+package mohammadali.fouladi.n01547173.mf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+// MohammadAli Fouladi N01547173
+
+public class MoeFouSplash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(MoeFouSplash.this, Mo1e.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
+    }
+}
